1c2ec2528ac57553a7c411cc438341dd8241bb12
libeanim <libeanim@users.noreply.github.com>
2021-10-13 16:04:13 +0200

    Implement proper config system

--- Resnet50New.py
+++ Resnet50New.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import json
 import torch
 from torch import nn
 from torchvision import transforms
@@ -13,8 +15,6 @@ from datetime import datetime
 assert torch.cuda.is_available()
 print('CUDA is availbale', torch.cuda.is_available())
 
-torch.manual_seed(42)
-
 
 # Generate id to reference files/plots/images
 dt_id = datetime.now().isoformat()
@@ -23,6 +23,7 @@ print(f'ID: {dt_id}')
 # General output directory
 RESULT_DIR = f'results/resnet50new/{dt_id}'
 os.mkdir(RESULT_DIR)
+print('Result dir:', RESULT_DIR)
 
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
@@ -100,8 +101,9 @@ class ResNet50NN(nn.Module):
         
         return x
 
-def get_accuracy(model, dataloader, cuda=True):
+def get_accuracy(model: nn.Module, dataloader: DataLoader, cuda: bool = True):
     accuracy, steps = 0, 0
+    model.eval()
     with torch.no_grad():
         for inputs, lables in dataloader:
 
@@ -116,9 +118,43 @@ def get_accuracy(model, dataloader, cuda=True):
         accuracy = accuracy / steps
         if cuda:
             accuracy = accuracy.cpu()
+    model.train()
     return accuracy.numpy()
 
+# LOAD CONFIG
+config = dict(
+    id = dt_id,
+    random_seed = 42,
+    model = "resnet50",
+    learning_rate = 0.001,
+    # learning_rate = 0.5,
+    momentum = 0.9,
+    epochs = 10,  # 18
+    log_steps = 100,
+    # k = 5,
+    weight_decay = 1e-4,
+    batch_size = 64,
+    base_model = None,
+    # base_model = "/home/bethge/ahochlehnert48/results/resnet50new/2021-09-21T10:29:43.000481_final.pth",
 
+    # Scheduler step size
+    # step_size = 100,
+    # scheduler = 'MultiStepLR',
+    # milestones = [30, 60, 90, 130, 150], #[10, 20, 30, 40, 50, 60]#[15, 30, 60, 90, 110, 140]
+    # gamma = 0.1
+)
+if len(sys.argv) == 2:
+    cfg_path = sys.argv[1]
+    if cfg_path == '--input':
+        new_config = json.loads(input('Paste JSON config: '))
+    elif os.path.exists(cfg_path):
+        print('Reading config file:', cfg_path)
+        with open(cfg_path, 'r') as cfg_file:
+            new_config = json.load(cfg_file)
+    else:
+        new_config = json.loads(cfg_path)
+    config.update(new_config)
+print('Config:', json.dumps(config, indent=2))
 # # Load imagenet subset for weights initialisation
 # train_latent = load_data('/home/bethge/ahochlehnert48/results/imnet_train_latent.npz')
 # val_latent = load_data('/home/bethge/ahochlehnert48/results/imnet_val_latent.npz')
@@ -143,38 +179,33 @@ def get_accuracy(model, dataloader, cuda=True):
 
 # print('Difference:', np.sum(val_latent[2]), 'of', np.size(val_latent[2]))
 
-
-import wandb
-wandb.init(project='resnet-50-mpl', entity='libeanim')
-# 2. Save model inputs and hyperparameters
-config = wandb.config
-config.id = dt_id
-# config.learning_rate = 0.001
-config.learning_rate = 0.5
-config.momentum = 0.9
-config.epochs = 10  # 18
-config.log_steps = 100
-config.k = 3
-config.weight_decay = 1e-4
-config.batch_size = 64
-
-# Scheduler step size
-config.step_size = 20
-config.gamma = 0.1
+torch.manual_seed(config["random_seed"])
 
 # Save config
 
 with open(f'{RESULT_DIR}/config.txt', 'w') as config_file: 
-    config_file.write(str(config.__dict__))
+    # config_file.write(str(config.__dict__))
+    json.dump(config, config_file)
 
 
 # Initialise weights
-# w = torch.Tensor(initialise_weights(config.k)).T
+# w = torch.Tensor(initialise_weights(config["k"])).T
 w = None
-model = ResNet50NN(k=config.k, init_pl=w)
+if config['model'] == "resnet50new":
+    model = ResNet50NN(k=config["k"], init_pl=w)
+elif config["model"] == "resnet50":
+    model = resnet50(pretrained=False)
+else:
+    raise ValueError(f'Model "{config["model"]}" does not exists')
 
-## Load pretrained model (only works with k=3)
-model.load_state_dict(torch.load('/home/bethge/ahochlehnert48/results/resnet50new/2021-09-21T10:29:43.000481_final.pth'))
+## Load pretrained model
+if config["base_model"] is not None:
+    model.load_state_dict(torch.load(config["base_model"]))
+
+
+
+import wandb
+wandb.init(project='resnet-50-mpl', entity='libeanim', config=config)
 
 # Delete latent data from memory and trigger garbage collector
 # del train_latent
@@ -200,20 +231,26 @@ val_dataset = ImageFolder(
     ])
 )
     
-train_loader = DataLoader(train_dataset, shuffle=True, batch_size=config.batch_size)
-val_loader = DataLoader(val_dataset, shuffle=True, batch_size=config.batch_size)
+train_loader = DataLoader(train_dataset, shuffle=True, batch_size=config["batch_size"])
+val_loader = DataLoader(val_dataset, shuffle=True, batch_size=config["batch_size"])
 
 
 wandb.watch(model)
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=config.learning_rate, momentum=config.momentum,
-                      weight_decay=config.weight_decay)
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config.step_size, gamma=config.gamma)
+optimizer = optim.SGD(model.parameters(), lr=config["learning_rate"], momentum=config["momentum"],
+                      weight_decay=config["weight_decay"])
+
+assert config['scheduler'] is None or config['scheduler'] == 'MultiStepLR'
+
+scheduler = None
+if config['scheduler'] == "MultiStepLR":
+    scheduler = optim.lr_scheduler.MultiStepLR(optimizer, config["milestones"], gamma=config["gamma"])
+# scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=config["step_size"], gamma=config["gamma"])
 model.cuda()
 
-for epoch in range(config.epochs):  # loop over the dataset multiple times
+for epoch in range(config["epochs"]):  # loop over the dataset multiple times
 
     running_loss = 0.0
     running_accuracy = 0
@@ -229,23 +266,31 @@ for epoch in range(config.epochs):  # loop over the dataset multiple times
         loss.backward()
         optimizer.step()
 
-        # learning rate decay
-        scheduler.step()
+        # # learning rate decay
+        # if scheduler is not None:
+        #     scheduler.step()
 
         # print statistics
         running_loss += loss.item()
         preditions = torch.argmax(outputs, axis=1)
         running_accuracy += (torch.sum(preditions == labels) / outputs.shape[0]).cpu().detach().numpy()
-        if i % config.log_steps == config.log_steps - 1:    # print every 2000 mini-batches
+        if i % config["log_steps"] == config["log_steps"] - 1:    # print every 2000 mini-batches
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / config.log_steps))
+                  (epoch + 1, i + 1, running_loss / config["log_steps"]))
             # test_accuracy = get_accuracy(model, val_loader)
-            wandb.log({"loss": running_loss / config.log_steps, "accuracy": running_accuracy / config.log_steps })
+            wandb.log({
+                "loss": running_loss / config["log_steps"],
+                "accuracy": running_accuracy / config["log_steps"],
+                "lr": optimizer.param_groups[0]['lr']
+            })
             running_loss = 0.0
             running_accuracy = 0
-
+        
+    # learning rate decay
+    if scheduler is not None:
+        scheduler.step()
     
-    torch.save(model.state_dict(), f"{RESULT_DIR}/{config.id}_{epoch}.pth")
+    torch.save(model.state_dict(), f"{RESULT_DIR}/{ config['id'] }_{epoch}.pth")
 # model.cpu()
-torch.save(model.state_dict(), f"{RESULT_DIR}/{config.id}_final.pth")
+torch.save(model.state_dict(), f"{RESULT_DIR}/{ config['id'] }_final.pth")
 print('Finished Training')
